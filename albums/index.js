d495fcec287a28d15ed01f9a67149e7ad5a66cf6
Petemab <peteben18@hotmail.com>
2018-09-08 19:27:30 +0100

    onto the next exercise

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,14 @@
 // import a library to help create a component //
+import React from 'react';
 
+import { Text, AppRegistry} from 'react-native';
 
 // create a component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
 
 
 // render component to the device //
+AppRegistry.registerComponent('albums', () => App);
