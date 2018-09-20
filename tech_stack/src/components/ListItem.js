34659e2d9f7815f767d2e54151967fe5a9c46289
Petemab <peteben18@hotmail.com>
2018-09-20 15:14:21 +0100

    this tutorial finished.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+
+
+  renderDescription() {
+    const { expanded } = this.props;
+    const library  = this.props.library.item;
+
+    if ( expanded ){
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
+
   render() {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library.item;
 
-    console.log(this.props.library);
+
+
     return(
-
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.item.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -26,4 +63,12 @@ const styles = {
   }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+  // this refacrorting makes no sense to me on first watch
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+  return { expanded };
+};
+
+//first argument is mapStateToProps- but it's null, second is actiosn
+export default connect(mapStateToProps, actions)(ListItem);
